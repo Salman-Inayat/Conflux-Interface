16ec860f46ce2d429348e4dfbeb5987b24e1f987
Salman-Inayat <salmaninayat1999@gmail.com>
2021-04-27 17:51:36 +0500

    fetching data from json-server

--- src/Components/SankeyChart.js
+++ src/Components/SankeyChart.js
@@ -3,6 +3,7 @@ import Chart from "react-google-charts";
 import Grid from '@material-ui/core/Grid';
 
 const SankeyChart = (props) => {
+
   return (
     <Grid item md={9}>
       <Chart
@@ -19,32 +20,9 @@ const SankeyChart = (props) => {
         //     },
         //   },
         // }}
-        data={[
-          ["From", "To", "Weight"],
-          ["confirm_email", "signup_form", 5],
-          ["confirm_email", "product_page", 1],
-          ["confirm_email", "search", 7],
-
-          ["signup_form", "landing", 2],
-          ["signup_form", "imput_email", 1],
-          ["signup_form", "view_more_details", 1],
-          ["product_page", "apply_filters", 1],
-          ["product_page", "payment_info", 3],
-          ["product_page", "view_more_details", 1],
-          ["search", "payment_info", 1],
-          ["search", "view_more_details", 1],
-          ["search", "payment_success", 2],
-          ["search", "landing", 3],
-          ["landing", "apply_filters", 1],
-          ["landing", "input_password", 3],
-          ["payment_info", "input_password", 3],
-          ["payment_info", "input_email", 3],
-          ["view_more_details", "apply_filters", 1],
-         
-        ]}
+        data={props.text}
         rootProps={{ "data-testid": "2" }}
       />
-      <p>{props.text}</p>
     </Grid>
   );
 };
--- src/Components/SideBar.js
+++ src/Components/SideBar.js
@@ -9,10 +9,11 @@ import Paper from "@material-ui/core/Paper";
 import SankeyChart from "./SankeyChart";
 import axios from "axios";
 
-
 const SideBar = () => {
   const [labelvalue, setLabelValue] = useState("confirm_email");
-  const [data, setdata] = useState({})
+  const [data, setData] = useState([[]]);
+  const [demo, setDemo] = useState();
+  const [demoData, setDemoData] = useState([]);
 
   const labels = [
     "confirm_email",
@@ -29,27 +30,99 @@ const SideBar = () => {
     "view_more_details",
   ];
 
-  useEffect( () => {
-    console.log(data)
- },
- [data]
- )
+  useEffect(() => {
+    console.log(demoData);
+  }, [setDemoData]);
+
+  const chartData = [
+    ["From", "To", "Weight"],
+    ["demo", "signup_form", 4],
+    ["demo", "product_page", 5],
+    ["demo", "search", 7],
+
+    ["signup_form", "landing", 2],
+    ["signup_form", "imput_email", 1],
+    ["signup_form", "view_more_details", 1],
+    ["product_page", "apply_filters", 1],
+    ["product_page", "payment_info", 3],
+    ["product_page", "view_more_details", 1],
+    ["search", "payment_info", 1],
+    ["search", "view_more_details", 1],
+    ["search", "payment_success", 2],
+    ["search", "landing", 3],
+    ["landing", "apply_filters", 1],
+    ["landing", "input_password", 3],
+    ["payment_info", "input_password", 3],
+    ["payment_info", "input_email", 3],
+    ["view_more_details", "apply_filters", 1],
+  ];
+
+    let data_keys = []
+    let data_values = []
+    let keys_array = []
+    let final_data = []
 
   function handleChange(event) {
-    setLabelValue(event.target.value);
+    const clicked_event = event.target.value;
+    setLabelValue(clicked_event);
+    setDemo("clicked");
 
-    axios.get('https://jsonplaceholder.typicode.com/posts')
-        .then(function (response) {
-            let response_data  = response.data
-            setdata(response_data);
-        })
+    axios
+      .get("http://localhost:3001/" + clicked_event)
+      .then(function (response) {
+        let i;
+        let response_data = response.data;
+        const parsed_data = JSON.parse(JSON.stringify(response_data));
+        data_keys = Object.keys(parsed_data[0][0])
+
+        for (i = 0; i < parsed_data[0].length; i++) {
+          data_values.push(Object.values(parsed_data[0][0]))
+        }
+
+        // for (i = 0; i < data_keys.length; i++){
+        //   keys_array = (data_keys[i])
+        // }
+
+        final_data  = data_keys.concat(data_values)
+
+        console.log(final_data)  
+      });
+
+    //   function arrayToJSONObject (arr){
+    //     var keys = arr[0];
+    //     var newArr = arr.slice(1, arr.length);
+
+    //     var formatted = [],
+    //     data = newArr,
+    //     cols = keys,
+    //     l = cols.length;
+    //     for (var i=0; i<data.length; i++) {
+    //             var d = data[i],
+    //                     o = {};
+    //             for (var j=0; j<l; j++)
+    //                     o[cols[j]] = d[j];
+    //             formatted.push(o);
+    //     }
+    //     return formatted;
+    // }
+
+    //   const return_object = arrayToJSONObject(chartData);
+    //   console.log(return_object)
+
+    //   axios
+    //   .post("http://localhost:3001/demo", return_object)
+    //   .then(function (response) {
+    //     console.log("posted")
+    //   });
   }
 
   return (
     <Grid container spacing={2}>
       <Grid item md={4} style={{ marginLeft: "20px" }}>
         <Paper>
-          <FormLabel style={{margin: "10px 35px"}} color="primary ">Search For</FormLabel>
+          <FormLabel style={{ margin: "10px 35px" }} color="primary ">
+            Search For
+          </FormLabel>
           <RadioGroup
             name="spacing"
             aria-label="spacing"
@@ -63,14 +136,14 @@ const SideBar = () => {
                 value={value}
                 control={<Radio />}
                 label={value}
-                style={{margin: "-5px 20px"}}
+                style={{ margin: "-5px 20px" }}
               />
             ))}
           </RadioGroup>
         </Paper>
       </Grid>
       <Grid item md={7}>
-        <SankeyChart text={labelvalue} />
+        {demo ? <SankeyChart text={final_data} /> : <SankeyChart text={chartData} />}
       </Grid>
     </Grid>
   );
