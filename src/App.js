0926f17c4f705c7137b7985078fa696b8f613e98
Salman-Inayat <salmaninayat1999@gmail.com>
2021-05-01 22:48:49 +0500

    Added graph support via Heroku

--- src/App.js
+++ src/App.js
@@ -2,13 +2,33 @@ import React from "react";
 import SideBar from "./Components/SideBar";
 import Grid from "@material-ui/core/Grid";
 import Header from "./Components/Header";
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  HttpLink,
+} from "@apollo/client";
 // import Customer_Card from "./Components/Customer_Card"
 
+const createApolloClient = () => {
+  return new ApolloClient({
+    link: new HttpLink({
+      uri: "http://localhost:8080/v1/graphql",
+      // // headers: {
+      //   Authorization: `Bearer ${authToken}`
+      // // }
+    }),
+    cache: new InMemoryCache(),
+  });
+};
+
 const App = () => {
+  const client = createApolloClient();
   return (
+    <ApolloProvider client={client}>
       <Grid container spacing={2}>
-        <Grid md = {12} xs={12}>
-          <Header/>
+        <Grid md={12} xs={12}>
+          <Header />
         </Grid>
         <Grid md={12}>
           <SideBar />
@@ -20,6 +40,7 @@ const App = () => {
         <Customer_Card/>
         </Grid> */}
       </Grid>
+    </ApolloProvider>
   );
 };
 
