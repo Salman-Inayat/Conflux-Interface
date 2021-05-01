1be4044a1833d57cb209637de230b7df648549e3
Salman-Inayat <salmaninayat1999@gmail.com>
2021-05-01 22:49:15 +0500

    Added support for multiple graphQL queries

--- src/Components/SideBar.js
+++ src/Components/SideBar.js
@@ -7,12 +7,71 @@ import RadioGroup from "@material-ui/core/RadioGroup";
 import Radio from "@material-ui/core/Radio";
 import Paper from "@material-ui/core/Paper";
 import SankeyChart from "./SankeyChart";
-import axios from "axios";
+// import axios from "axios";
+// import { gql } from "@apollo/client";
+import { gql, useLazyQuery } from "@apollo/client";
+
+const GET_CONFIRM_EMAIL = gql`
+  query {
+    confirm_email {
+      From
+      To
+      Weight
+    }
+  }
+`;
+
+const GET_SEARCH = gql`
+  query {
+    search {
+      From
+      To
+      Weight
+    }
+  }
+`;
 
 const SideBar = () => {
-  const [labelvalue, setLabelValue] = useState("confirm_email");
-  const [data, setData] = useState([[]]);
+  let data_keys = [[]];
+  let data_values = [];
+  let final_data = [];
+  const [labelvalue, setLabelValue] = useState("");
   const [demo, setDemo] = useState();
+  const [finalData, setFinalData] = useState([[]]);
+
+  const [getConfirmEmail, { c_loading, c_error, c_data }] = useLazyQuery(
+    GET_CONFIRM_EMAIL, {
+      onCompleted: (somedata) => {
+        let response_data = somedata.confirm_email;
+        const parsed_data = JSON.parse(JSON.stringify(response_data));
+        data_values = parsed_data.map((x) => Object.values(x));
+        for (let x = 0; x < data_values.length; x++) {
+          data_values[x].splice([0], 1);
+        }
+  
+        final_data = data_keys.concat(data_values);
+        final_data[0] = ["From", "To", "Weight"];
+        setFinalData(final_data);
+      },
+    }
+  );
+
+  const [getSearch, { s_loading, s_error, s_data }] = useLazyQuery(
+    GET_SEARCH,{
+      onCompleted: (somedata) => {
+        let response_data = somedata.search;
+        const parsed_data = JSON.parse(JSON.stringify(response_data));
+        data_values = parsed_data.map((x) => Object.values(x));
+        for (let x = 0; x < data_values.length; x++) {
+          data_values[x].splice([0], 1);
+        }
+  
+        final_data = data_keys.concat(data_values);
+        final_data[0] = ["From", "To", "Weight"];
+        setFinalData(final_data);
+      },
+    }
+  );
 
   const labels = [
     "confirm_email",
@@ -34,7 +93,6 @@ const SideBar = () => {
     ["demo", "signup_form", 4],
     ["demo", "product_page", 5],
     ["demo", "search", 7],
-
     ["signup_form", "landing", 2],
     ["signup_form", "imput_email", 1],
     ["signup_form", "view_more_details", 1],
@@ -52,66 +110,41 @@ const SideBar = () => {
     ["view_more_details", "apply_filters", 1],
   ];
 
-    let data_keys = [[]]
-    let data_values = []
-    let final_data = []
+  useEffect(() => {
+    return () => {
+    };
+  }, [finalData]);
 
   function handleChange(event) {
     const clicked_event = event.target.value;
     setLabelValue(clicked_event);
     setDemo("clicked");
+    // getServerData();
 
-    axios
-      .get("http://localhost:3001/" + clicked_event)
-      .then(function (response) {
-        let i;
-        let response_data = response.data;
-
-        const parsed_data = JSON.parse(JSON.stringify(response_data));
-        data_keys[0].push(Object.keys(parsed_data[0][0]))
-
-        for (i = 0; i < parsed_data[0].length; i++) {
-          data_values.push(Object.values(parsed_data[0][i]))
-        }
-
-        final_data  = data_keys.concat(data_values)
-        final_data[0] = ["From", "To", "Weight"]
-        setData(final_data)
-
-      });
-      
-    //   function arrayToJSONObject (arr){
-    //     var keys = arr[0];
-    //     var newArr = arr.slice(1, arr.length);
-
-    //     var formatted = [],
-    //     data = newArr,
-    //     cols = keys,
-    //     l = cols.length;
-    //     for (var i=0; i<data.length; i++) {
-    //             var d = data[i],
-    //                     o = {};
-    //             for (var j=0; j<l; j++)
-    //                     o[cols[j]] = d[j];
-    //             formatted.push(o);
-    //     }
-    //     return formatted;
-    // }
-
-    //   const return_object = arrayToJSONObject(chartData);
-    //   console.log(return_object)
-
-    //   axios
-    //   .post("http://localhost:3001/demo", return_object)
-    //   .then(function (response) {
-    //     console.log("posted")
-    //   });
+    switch (event.target.value) {
+      case ("confirm_email"):
+        getConfirmEmail(event.target.value);
+        break
+      case ("search"):
+        getSearch(event.target.value);
+        break;
+      default:
+        console.log("select correct option")
+    }
   }
+
+  // if (loading) {
+  //   return <div>Loading...</div>;
+  // }
+  // if (error) {
+  //   console.error(error);
+  //   return <div style={{ marginLeft: "30px" }}>Error!</div>;
+  // }
 
   return (
     <Grid container spacing={2}>
-      <Grid item md={3} style={{ marginLeft: "50px"}}>
-        <Paper style={{backgroundColor:'#cccccc',  padding: '20px ' }}>
+      <Grid item md={3} style={{ marginLeft: "50px" }}>
+        <Paper style={{ backgroundColor: "#cccccc", padding: "20px " }}>
           <FormLabel style={{ margin: "10px 35px" }} color="primary ">
             Select an Event
           </FormLabel>
@@ -128,17 +161,19 @@ const SideBar = () => {
                 value={value}
                 control={<Radio />}
                 label={value}
-                style={{ margin: "-5px 20px", color:'black' }}
+                style={{ margin: "-5px 20px", color: "black" }}
               />
             ))}
           </RadioGroup>
         </Paper>
       </Grid>
-      <Grid item md={1}>
-
-      </Grid>
-      <Grid item md={7} style={{padding:'50px'}}>
-        {demo ? <SankeyChart text={data} /> : <SankeyChart text={chartData} />}
+      <Grid item md={1}></Grid>
+      <Grid item md={7} style={{ padding: "50px" }}>
+        {demo ? (
+          <SankeyChart text={finalData} />
+        ) : (
+          <SankeyChart text={chartData} />
+        )}
       </Grid>
     </Grid>
   );
