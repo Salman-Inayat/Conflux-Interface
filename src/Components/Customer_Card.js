d5b4835e9a7dabea53f4677f203bd509a2d04876
Salman-Inayat <salmaninayat1999@gmail.com>
2021-05-11 12:25:36 +0500

    Updated the app

--- src/Components/Customer_Card.js
+++ src/Components/Customer_Card.js
@@ -5,18 +5,21 @@ import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Typography from "@material-ui/core/Typography";
 import Grid from "@material-ui/core/Grid";
+import FormControl from "@material-ui/core/FormControl";
+import InputLabel from "@material-ui/core/InputLabel";
+import Input from "@material-ui/core/Input";
 
 const useStyles = makeStyles({
   root: {
     maxWidth: 345,
     margin: "30px auto",
-    padding:'10px 20px',
+    padding: "10px 20px",
     backgroundColor: "#e0d6d6",
   },
   media: {
     height: 70,
     width: 70,
-    margin: '20px 0px 20px 20px',
+    margin: "20px 0px 20px 20px",
     borderRadius: "5px",
   },
   history_images: {
@@ -25,16 +28,32 @@ const useStyles = makeStyles({
     margin: "5px",
     borderRadius: "5px",
   },
-  details:{
-      margin: '0px 0px 30px 0px',
+  details: {
+    margin: "0px 0px 30px 0px",
   },
-  line:{
-      borderBottom: '1px solid grey',
-  }
+  line: {
+    borderBottom: "1px solid grey",
+  },
+  formClass: {
+    marginTop: "30px",
+    display: "flex",
+    // justifyContent:'center'
+  },
+  inputField: {
+    width: "200px",
+    height: "20px",
+    marginRight: "10px",
+    border: "1px solid #d263aa",
+    borderRadius: "8px",
+  },
+  submitButton: {
+    border: "1px solid #d263aa",
+    borderRadius: "15px",
+    height: "25px",
+  },
 });
 
 export default function Customer_Card(props) {
-
   const classes = useStyles();
   const p_history = [
     "https://images.unsplash.com/photo-1515378791036-0648a3ef77b2?ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
@@ -58,40 +77,39 @@ export default function Customer_Card(props) {
               Customer ID:
             </Typography>
           </Grid>
-          <Grid  className={classes.line} item xs={3}>
+          <Grid className={classes.line} item xs={3}>
             <Typography variant="body2" color="textSecondary" component="p">
               XYZ
               {/* {props.id} */}
             </Typography>
           </Grid>
-          <Grid  className={classes.line} item xs={6}>
+          <Grid className={classes.line} item xs={6}>
             <Typography variant="body2" color="textSecondary" component="p">
               Customer Type:
             </Typography>
           </Grid>
-          <Grid  className={classes.line} item xs={3}>
+          <Grid className={classes.line} item xs={3}>
             <Typography variant="body2" color="textSecondary" component="p">
               Old
               {/* {props.customer_type} */}
             </Typography>
           </Grid>
-          <Grid  className={classes.line} item xs={6}>
+          <Grid className={classes.line} item xs={6}>
             <Typography variant="body2" color="textSecondary" component="p">
               Total Visits:
             </Typography>
           </Grid>
-          <Grid  className={classes.line} item xs={3}>
+          <Grid className={classes.line} item xs={3}>
             <Typography variant="body2" color="textSecondary" component="p">
-              4
-              {/* {props.visits} */}
+              4{/* {props.visits} */}
             </Typography>
           </Grid>
-          <Grid  className={classes.line} item xs={6}>
+          <Grid className={classes.line} item xs={6}>
             <Typography variant="body2" color="textSecondary" component="p">
               Average Spending:
             </Typography>
           </Grid>
-          <Grid  className={classes.line} item xs={3}>
+          <Grid className={classes.line} item xs={3}>
             <Typography variant="body2" color="textSecondary" component="p">
               Rs 500
               {/* {props.average_spending} */}
@@ -105,15 +123,19 @@ export default function Customer_Card(props) {
           <Grid item xs={6}>
             <Typography variant="body2" color="textSecondary" component="p">
               Rs 2000
-            {/* {props.total_spending} */}
+              {/* {props.total_spending} */}
             </Typography>
           </Grid>
         </Grid>
 
-        <Typography  color="textSecondary" variant="p" style={{marginLeft:'5px'}}>
+        <Typography
+          color="textSecondary"
+          variant="p"
+          style={{ marginLeft: "5px" }}
+        >
           Purchase History:
         </Typography>
-        <div style={{ display: "flex", marginBottom:'20px' }}>
+        <div style={{ display: "flex", marginBottom: "20px" }}>
           {p_history.map((value) => (
             <CardMedia
               className={classes.history_images}
@@ -123,7 +145,11 @@ export default function Customer_Card(props) {
           ))}
         </div>
 
-        <Typography  color="textSecondary" variant="p" style={{marginLeft:'5px'}}>
+        <Typography
+          color="textSecondary"
+          variant="p"
+          style={{ marginLeft: "5px" }}
+        >
           Customer might be interested in
         </Typography>
         <div style={{ display: "flex" }}>
@@ -135,6 +161,17 @@ export default function Customer_Card(props) {
             />
           ))}
         </div>
+        <br />
+        <hr />
+        <form action="/" method="POST" className={classes.formClass}>
+          <input
+            type="text"
+            placeholder="Enter name"
+            className={classes.inputField}
+            required
+          />
+          <button className={classes.submitButton}>Submit</button>
+        </form>
       </CardContent>
     </Card>
   );
